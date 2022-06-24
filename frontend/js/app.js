5d5aeef331cf36f75bfe5c0604709ba8c9bae9df
Dannilo Franco <dannilofranco@gmail.com>
2022-06-23 21:30:49 -0300

    from ether to matic

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -558,7 +558,8 @@ async function loadInfo() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
-  const price = web3.utils.fromWei(info.deploymentConfig.mintPrice, 'ether');
+  // coloquei um MATIC aqui
+  const price = web3.utils.fromWei(info.deploymentConfig.mintPrice, 'matic');
   const pricePerMint = document.getElementById("pricePerMint");
   const maxPerMint = document.getElementById("maxPerMint");
   const totalSupply = document.getElementById("totalSupply");
